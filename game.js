5be65634429145f2110dc22b996d4b37ec4978cb
DeepN-Code <dipanranam@gmail.com>
2021-11-01 13:24:04 +0545

    Changes in JS and HTML files.

--- game.js
+++ game.js
@@ -1,96 +1,72 @@
+let playerSelection;
+let computerSelection;
+let playerWin=0;
+let computerWin=0;
+
 function computerPlay() {
     let arr = ["rock", "paper", "scissors"];
     return arr[Math.floor(Math.random() * arr.length)];
     console.log(arr[Math.floor(Math.random() * arr.length)]);
 }
 
-function playerInput() {
-    let inp = prompt("Enter your choice (Rock, Paper or Scissors): ").toLowerCase();
-    if (inp == "rock" || inp == "paper" || inp == "scissors") {
-        console.log(inp);
-        return inp;
-    }
-    else {
-        alert("Enter valid input!!!")
-    }
-
-}
-
-/*
-let playerSelection = playerInput();
-let computerSelection = computerPlay();
-playerSelection, computerSelection
-*/
-
-function playRound() {
-    let playerSelection = playerInput();
+function playRound(playerSelection) {
     let computerSelection = computerPlay();
 
-    const r1 = "It's a tie!";
-    const r2 = "You lost! Paper beats Rock.";
-    const r3 = "You won! Rock beats Scissors.";
-    const r4 = "You won! Paper beats Rock.";
-    const r5 = "You lost! Scissors beats Paper.";
-    const r6 = "You lost! Rock beats Scissors.";
-    const r7 = "You won! Scissors beats Paper.";
-
     if (playerSelection == computerSelection) {
-        console.log(r1);
-        return r1;
+        declareResult(`Draw! Score: ${playerWin} - ${computerWin}`);
+        console.log(`Draw! Score: ${playerWin} - ${computerWin}`);
     }
-    else if (playerSelection == "rock" && computerSelection == "paper") {
-        console.log(r2);
-        return r2;
+    else if ((playerSelection == "rock" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "rock")) {
+        computerWin++;
+        declareResult(`Round Lost! ${computerSelection} beats ${playerSelection}. Score: ${playerWin} - ${computerWin}`);
+        console.log(`Round Lost! ${computerSelection} beats ${playerSelection}. Score: ${playerWin} - ${computerWin}`);
     }
-    else if (playerSelection == "rock" && computerSelection == "scissors") {
-        console.log(r3);
-        return r3;
+    else if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper")) {
+        playerWin++;
+        declareResult(`Round Won! ${playerSelection} beats ${computerSelection}. Score: ${playerWin} - ${computerWin}`);
+        console.log(`Round Won! ${playerSelection} beats ${computerSelection}. Score: ${playerWin} - ${computerWin}`);
     }
-    else if (playerSelection == "paper" && computerSelection == "rock") {
-        console.log(r4);
-        return r4;
-    }
-    else if (playerSelection == "paper" && computerSelection == "scissors") {
-        console.log(r5);
-        return r5;
-    }
-    else if (playerSelection == "scissors" && computerSelection == "rock") {
-        console.log(r6);
-        return r6;
-    }
-    else if (playerSelection == "scissors" && computerSelection == "paper") {
-        console.log(r7);
-        return r7;
-    }
+    
 }
 
-function game() {
-    let playerWin = 0;
-    let computerWin = 0;
+const div = document.querySelector('.result');
 
-    for (let i = 0; i < 5; i++) {
-        let output = playRound();
+function declareResult(string){
+    
+    const result= document.createElement("p");
+    result.textContent= string;
+    div.appendChild(result);
 
-        if (output.search('won') >= 0) {
-            playerWin++;
-            console.log(`${output} Score: ${playerWin} - ${computerWin}`);
-        }
-        else if (output.search('lost') >= 0) {
-            computerWin++;
-            console.log(`${output} Score: ${playerWin} - ${computerWin}`);
-        }
-        else {
-            console.log(`${output} Score: ${playerWin} - ${computerWin}`);
-        }
-    }
-
-    if (computerWin > playerWin) {
-        console.log(`LOSER!!! Score: ${playerWin} - ${computerWin}`);
-    }
-    else if (playerWin > computerWin) {
-        console.log(`WINNER!!! Score: ${playerWin} - ${computerWin}`);
-    }
-    else {
-        console.log(`DRAW!!! Score: ${playerWin} - ${computerWin}`);
-    }
 }
+
+const outcome= document.querySelector(".outcome");
+
+function declareWinner(){
+    if(computerWin>playerWin)
+        outcome.textContent= "CPU wINS!!!"
+    else
+        outcome.textContent= "YOU WIN!!!"
+    
+    const resetBtn = document.createElement('button');
+    resetBtn.innerText= "Play Again!";
+    outcome.appendChild(resetBtn);
+
+    resetBtn.addEventListener('click', () => {
+        window.location.reload();
+    })
+
+}
+
+
+
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        playerSelection= btn.getAttribute('id').toLocaleLowerCase();
+
+        playRound(playerSelection);
+
+        if(playerWin==5 || computerWin==5)
+            declareWinner();
+    })
+});
